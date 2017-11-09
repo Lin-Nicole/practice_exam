d044c42965c958d72f7b96d5a5018705a0c5aede
Lin-Nicole <32175483+Lin-Nicole@users.noreply.github.com>
2017-11-09 11:36:45 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,14 +2,16 @@
 1.  Write a program that lists the numbers
     from 0-10 on separate lines
 """
-
+for i in range (11): 
+    print (i)
 
 
 """
 2.  Write a program that counts down from
    100 by 5's
 """
-
+for i in range (100, -1, -5)
+    print (i)
 
 
 """
@@ -18,6 +20,12 @@ This Function should take the value x and
 Return 3 times that number. Call the function
 and print the result to the screen for x = 20
 """
+
+def Triple(x):
+    x = x * 3
+    return x
+
+print (Triple(20))
 
 
 
@@ -28,6 +36,8 @@ to input a number, triple it and  display
 the result.  You will NEED print statements
 AND you may use your function above.
 """
+x = int(input('Please enter a number: '))
+print (Triple(x)) 
 
 
 
@@ -37,3 +47,4 @@ for the equation on the board:  Run this function
 and print the results for the inputs:
 (-3, -2, -1, 0, 1, 2, 3)
 """
+
